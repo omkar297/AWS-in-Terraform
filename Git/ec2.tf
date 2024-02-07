aaad5bdb66d8fca4a920802565305559b19e7bcd
Omkar Rajesh Rathod <60773492+omkar297@users.noreply.github.com>
2024-02-07 17:59:49 +0530

    variable fill added

--- Git/ec2.tf
+++ Git/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test-linux" {
   ami = "ami-03f4878755434977f"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   key_name = "0mK4r"
   associate_public_ip_address = true
   user_data = file("./script.sh")
