b04974c1cf81b2998dc400255eac4dcca76f6da7
Omkar Rajesh Rathod <60773492+omkar297@users.noreply.github.com>
2024-02-07 13:15:50 +0530

    git
    example of git also added

---
+++ Git/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "test-linux" {
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+  key_name = "0mK4r"
+  associate_public_ip_address = true
+  user_data = file("./script.sh")
+  tags = {
+    Name = "test-linux"
+    env = "test"
+    os = "linux-ubuntu"
+  }
+}
---
+++ Git/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt upgrade -y
+apt install git -y
+git -version
